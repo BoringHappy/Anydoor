3e01af63de4dc51dbe04ad4d49a4c63cc84f0b14
veinkr <yhf2lj@gmail.com>
2024-05-04 22:38:09 +0800

    remove keepass

--- anydoor/utils/__init__.py
+++ anydoor/utils/__init__.py
@@ -1,6 +1,5 @@
 from .check import check
 from .secret import Secret
-from .keepass import KeePass
 from .proxy import Proxy
 from .container import Container
 from .singleton import SingletonType
@@ -10,7 +9,6 @@ from .log import logger
 __all__ = [
     "check",
     "Secret",
-    "KeePass",
     "Proxy",
     "Container",
     "SingletonType",
