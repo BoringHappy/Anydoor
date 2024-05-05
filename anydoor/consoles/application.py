469a2f311aef2b220df5bd5fcca1ad5b9fd4ada1
veinkr <yhf2lj@gmail.com>
2024-05-05 12:44:58 +0800

    Remove keepass

--- anydoor/consoles/application.py
+++ anydoor/consoles/application.py
@@ -1,14 +1,12 @@
 from cleo.application import Application
 
 from .secrets import SecretCommand
-from .keepass import KeePassCommand
 from .chat import ChatCommand
 
 
 def main() -> int:
     application = Application()
     application.add(SecretCommand())
-    application.add(KeePassCommand())
     application.add(ChatCommand())
     exit_code: int = application.run()
     return exit_code
