40ac3656eb46b8a86f829aaddd65e735021df72d
veinkr <yhf2lj@gmail.com>
2024-09-01 22:03:38 +0800

    update util to __init__ for dbs

--- anydoor/dbs/__init__.py
+++ anydoor/dbs/__init__.py
@@ -1,1 +1,11 @@
-__all__ = []
+from .postgres import Postgres
+from .clickhouse import Clickhouse
+from .sql_runner import SQLRunner
+from .sqlite import Sqlite
+
+__all__ = [
+    Postgres,
+    Clickhouse,
+    Sqlite,
+    SQLRunner,
+]
