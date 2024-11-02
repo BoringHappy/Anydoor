bc7750bc62efc5b38abe4a22f16e622ec0f8ce36
veinkr <yhf2lj@gmail.com>
2024-11-02 21:03:35 +0800

    Add get_s3_client

--- anydoor/utils/s3_file_sync.py
+++ anydoor/utils/s3_file_sync.py
@@ -3,6 +3,7 @@ import os
 from cloudpathlib import S3Client
 import shutil
 from contextlib import contextmanager
+from functools import lru_cache
 
 
 @contextmanager
@@ -23,3 +24,13 @@ def S3FileSync(s3_path, local_path, secret_name: str = None, clean: bool = True)
         cloud_path.upload_from(local_path)
         if clean:
             shutil.rmtree(local_path)
+
+
+@lru_cache
+def get_s3_client(secret_name: str = None):
+    secret = Vault().get(secret_name or os.environ["MINIO_TOKEN_NAME"])
+    return S3Client(
+        aws_access_key_id=secret.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=secret.AWS_SECRET_ACCESS_KEY,
+        endpoint_url=secret.AWS_ENDPOINT,
+    )
