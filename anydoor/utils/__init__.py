ae4290a7c9f80c36ba54aaf52a3ff3677f59cb73
veinkr <yhf2lj@gmail.com>
2025-04-17 13:14:57 +0000

    change log to loguru

--- anydoor/utils/__init__.py
+++ anydoor/utils/__init__.py
@@ -1,4 +1,3 @@
-from .log import logger
-
+from loguru import logger
 
 __all__ = ["logger"]
--- anydoor/utils/log.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-from loguru import logger
-
-logger.add(
-    sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>"
-)
