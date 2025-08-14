8e8aac1642a56c9d699d7e8e9aa5e1ce7fd77d39
veinkr <yhf2lj@gmail.com>
2025-08-14 20:50:43 +0800

    Add usual func to main path

--- anydoor/__init__.py
+++ anydoor/__init__.py
@@ -1,1 +1,12 @@
-__all__ = []
+from .utils.cache import cache_db
+from .utils.config import load_config, load_hydra_config
+from .utils.proxy import Proxy
+from .utils.vault import Vault
+
+__all__ = [
+    "load_config",
+    "load_hydra_config",
+    "cache_db",
+    "Proxy",
+    "Vault",
+]
