564d2284e6167bdfbd13a4e0a51c7c0b2318777e
veinkr <yhf2lj@gmail.com>
2025-08-21 10:27:12 +0800

    Add type stubs for requests, pandas, and hvac; update development dependencies in pyproject.toml and uv.lock

--- anydoor/dbs/__init__.py
+++ anydoor/dbs/__init__.py
@@ -5,11 +5,11 @@ from .spark import init_spark
 from .sql_runner import SQLRunner
 from .sqlite import Sqlite
 
-__all__ = [
-    Postgres,
-    Clickhouse,
-    Sqlite,
-    SQLRunner,
-    Kafka,
-    init_spark,
-]
+__all__ = (
+    "Postgres",
+    "Clickhouse",
+    "Sqlite",
+    "SQLRunner",
+    "Kafka",
+    "init_spark",
+)
